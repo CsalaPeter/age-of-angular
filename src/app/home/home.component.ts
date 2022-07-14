0e7271b41c64af401fd56ba5d3e0768ba8c90076
CsalaPeter <csalapeter95@gmail.com>
2022-07-14 14:51:22 +0200

    Added footer

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['../../assets/css/home.component.css'],
+  styleUrls: [
+    '../../assets/css/home.component.css',
+    '../../assets/css/footer.css',
+  ],
 })
 export class HomeComponent implements OnInit {
   constructor() {}
